45b9463dd35a3e0cb7be4307e591da4925185e0f
Rahul <107238271+iyumrahul@users.noreply.github.com>
2022-10-12 00:19:51 +0000

    added job item

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__,template_folder='templates')
 
-
+JOBS=[
+  {
+    'Company':'KPMG',
+    'title':'Junior Data Analyst',
+    'Salary':'7.6LPA',
+    'Location': 'Bengaluru, Karnataka'
+  },
+  {
+    'Company':'Walmart',
+    'title':'Data Scientist',
+    'Salary':'13LPA',
+    'Location': 'Delhi, India'
+  },
+  {
+    'Company':'Currenex State Street Trust Company',
+    'title':'Data Analyst',
+    'Location': 'Bengaluru, Karnataka'
+  },
+  {
+    'Company':'Shell',
+    'title':'Data Analyst-MDM',
+    'Salary':'4.2LPA',
+    'Location': 'Bengaluru, Karnataka'
+  }
+]
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
