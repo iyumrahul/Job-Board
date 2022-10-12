dfafd02a55140d05c7a17f68ef5a1b7edd184d5b
Rahul <107238271+iyumrahul@users.noreply.github.com>
2022-10-12 01:27:01 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__,template_folder='templates')
 
@@ -30,6 +30,10 @@ JOBS=[
 @app.route('/')
 def hello():
     return render_template('home.html',jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
